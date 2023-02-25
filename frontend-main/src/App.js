e6825e2a6ba5db7903067783042d21e58e1fefff
thesoulharmonic <thesoulharmonic@gmail.com>
2023-02-25 15:19:36 +0000

    index update

--- frontend-main/src/App.js
+++ frontend-main/src/App.js
@@ -33,7 +33,7 @@ import Hanakiv from "./pages/artists/Hanakiv";
 
 if (process.env.NODE_ENV === 'production'){
   app.use(express.static(path.join(__dirname, 'frontend-main/build')));
-app.get('*',(req,res)=> {res.sendFile(path.resolve(__dirname, 'gondwanarecords-frontend', 'build','index.html'));
+app.get('*',(req,res)=> {res.sendFile(path.resolve(__dirname, 'frontend-main/public/index.html'));
 });
 }
 
